0b28ffcbca8a453b00cbd066a2f2c149062d10c7
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-11-25 19:02:36 -0800

    Buzz works

--- chpt2/fizzBuzz.js
+++ chpt2/fizzBuzz.js
@@ -7,9 +7,9 @@
 function fizzBuzz () {
   var i = 1;
   while (i <= 100) {
-    if (i%3==0){
+    if (i % 3 == 0){
       console.log("Fizz");
-    } else if (i%5==0) {
+    } else if (i % 5 == 0) {
       console.log("Buzz");
     } else {
       console.log(i);
