25ddf74ede8ed31a4e1a22e3ab03d5d1baaf7d83
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-11-25 21:10:54 -0800

    brute force pseudocode attempt with console.logs()

--- chpt2/chessBoard.js
+++ chpt2/chessBoard.js
@@ -14,6 +14,24 @@
     # # # #
 */
 
+function chessBoard (size = 8) {
+  var board = "";
+  for (var i = 1; i <= size; i++) {
+    console.log("line: " + i);
+    for (var j = 0; j <= size; j++) {
+      if ((i + j) % 2 == 0) {
+        console.log("Character: " + j + " gets a '#' ");
+      } else {
+        console.log("Character: " + j + " gets a ' ' ");
+      }
+    }
+    console.log("new line");
+    // board += "\n";
+  }
+  return board;
+}
+console.log(chessBoard());
+
 /*
   When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
 */
