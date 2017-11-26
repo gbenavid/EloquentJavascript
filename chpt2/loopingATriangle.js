db61aab2db9b9db8d00abcbdbc5ce86db9f27af8
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-11-26 14:35:56 -0800

    test giphy in readme

--- chpt2/loopingATriangle.js
+++ chpt2/loopingATriangle.js
@@ -13,10 +13,11 @@ Write a loop that makes 7 calls to console.log to output the following triangle:
 */
 
 function makeTriangle () {
-    var str = "#";
-    for (var i = 0; i < 8; i++) {
-      console.log(str);
-      str += "#";
-    }
-}
-console.log(makeTriangle());
+      var str = "#";
+      for (var i = 0; i < 8; i++) {
+        console.log(str);
+        str += "#";
+      }
+  }
+  console.log(makeTriangle());
+  
