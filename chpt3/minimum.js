71a6bdf643265ac4d3d1f12978ee89217abb2f5d
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-11-25 21:33:43 -0800

    add prompt for minimum()

---
+++ chpt3/minimum.js
@@ -0,0 +1,10 @@
+/* 
+The previous chapter introduced the standard function Math.min that returns its smallest argument. We can do that ourselves now. Write a function min that takes two arguments and returns their minimum.
+
+// Your code here.
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+*/
