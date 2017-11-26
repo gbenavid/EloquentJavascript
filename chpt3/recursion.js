1fcb1609a7a5a9d663c04dd0c39463f16c710044
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-11-26 15:35:05 -0800

    added prompt for isEven() + threw in some console logs at the end

---
+++ chpt3/recursion.js
@@ -0,0 +1,13 @@
+/* 
+Here’s another way to define whether a positive whole number is even or odd:
+ Zero is even.
+One is odd.
+ For any other number N, its evenness is the same as N - 2. 
+The function should accept a number parameter and return a Boolean.
+*/
+
+function isEven () { }
+
+// console.log(isEven(75));
+// console.log(isEven(50));
+// console.log(isEven(-1));
