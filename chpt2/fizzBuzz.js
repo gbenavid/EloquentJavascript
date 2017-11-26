7a6399f60f99311d07366d5c7c53490edf5543e5
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-11-25 18:51:24 -0800

    add prompt for fizzBuzz problem

---
+++ chpt2/fizzBuzz.js
@@ -0,0 +1,10 @@
+/*
+  Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
+  For numbers divisible by 3, print "Fizz" instead of the number, 
+  and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+*/
+
+/*
+  ... Modify your program to print "FizzBuzz", for numbers that are divisible by both 3 and 5 
+  (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+*/
