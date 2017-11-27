bfda73c56bcbdd93a5e8c09a2d48092865b93347
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-11-27 15:39:28 -0800

    beef out prompt and structure calls at bottom or page.

--- chpt4/aList.js
+++ chpt4/aList.js
@@ -1,17 +1,26 @@
 /*
-arrayToList() builds up a data structure like the previous one when given [1, 2, 3] as argument, 
-and listToArray() produces an array from a list. 
-Also write prepend(el, list) which creates a new list that adds the element to the front of the list, 
-and nth(list, num) which returns the element at the given position in the list, 
-  or undefined when there is no such element.
-If you haven’t already, also write a recursive version of nth. 
+  * arrayToList() builds up a data structure like the previous one when given [1, 2, 3] as argument, 
+  * listToArray() produces an array from a list. 
+  * prepend(el, list) which creates a new list that adds the element to the front of the list, 
+  * nth(list, num) which returns the element at the given position in the list, 
+    or undefined when there is no such element. ***recursive version requested.
 */
+function arrayToList(array) {
+  
+}
 
-console.log(arrayToList([10, 20]));
-// → {value: 10, rest: {value: 20, rest: null}}
-console.log(listToArray(arrayToList([10, 20, 30])));
-// → [10, 20, 30]
-console.log(prepend(10, prepend(20, null)));
-// → {value: 10, rest: {value: 20, rest: null}}
-console.log(nth(arrayToList([10, 20, 30]), 1));
-// → 20
+console.log(arrayToList([10, 20])); 
+/*   
+{value: 10,
+rest: {value: 20, 
+      rest: null} 
+} */
+
+function listToArray(list) { }
+// console.log(listToArray(arrayToList([10, 20, 30]))); // → [10, 20, 30]
+
+function prepend (el, list) { }
+// console.log(prepend(10, prepend(20, null))); // → {value: 10, rest: {value: 20, rest: null}}
+
+function nth(list, num) { }
+// console.log(nth(arrayToList([10, 20, 30]), 1)); // → 20
