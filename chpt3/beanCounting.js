e35b7519997d747d62cd608c3b912dc4bd1b110c
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-11-26 16:43:02 -0800

    working solution for part two- count chars.

--- chpt3/beanCounting.js
+++ chpt3/beanCounting.js
@@ -5,12 +5,15 @@ countChar() behaves like countBs() but also takes a single character that is to 
  */
 
 function countBs(str) {
+  return countChar(str, "B");
+}
+
+function countChar(str, char) {
   var count = 0;
   for (var i = 0; i < str.length; i++) {
-    if (str[i] == 'B') {
+    if (str[i] == char) {
       count++;
     }
   }
   return count;
 }
-console.log(countBs("ginaB"));
