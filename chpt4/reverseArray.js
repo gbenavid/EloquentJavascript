9ea2cd4795ea8d5649bf1366a824e6ddf4ff2940
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-11-28 14:03:38 -0800

    remove comments and tidy up 🏡

--- chpt4/reverseArray.js
+++ chpt4/reverseArray.js
@@ -15,18 +15,14 @@ function reverseArray(array) {
   }
   return newArray;
 }
-// console.log(reverseArray(["A", "B", "C"])); // → ["C", "B", "A"];
 
 var arrayValue = [1, 2, 3, 4, 5];
-
 function reverseArrayInPlace(array) {
   for (var i = array.length - 1; i >= Math.floor(array.length/2); i -= 1) {
-    var temp = array[array.length - 1 - i] // 1
-    array[array.length - 1 - i] = array[i]; // *5*, 2, 3, 4, 5
-    array[i] = temp; // 5, 2, 3, 4, *1*
-    // i = 4; temp = 5
+    var temp = array[array.length - 1 - i];
+    array[array.length - 1 - i] = array[i];
+    array[i] = temp;
   }
   return array;
 }
 reverseArrayInPlace(arrayValue);
-// console.log(arrayValue); // → [5, 4, 3, 2, 1]
