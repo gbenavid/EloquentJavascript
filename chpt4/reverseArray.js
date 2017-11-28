8f4ece4c67050dd1112c792ea521542c134db244
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-11-28 13:18:20 -0800

    starting reverseArray(). Adding console.logs and comments and C&P the question from EloquentJS

--- chpt4/reverseArray.js
+++ chpt4/reverseArray.js
@@ -8,10 +8,16 @@ Which variant do you expect to be useful in more situations?
 Which one is more efficient?
 */
 
-function reverseArray(array) { }
+function reverseArray(array) {
+  var newArray = [];
+  for (var i = array.length - 1; i > 0; i--) {
+    console.log(array[i]);
+  }
+  return newArray;
+}
 console.log(reverseArray(["A", "B", "C"])); // → ["C", "B", "A"];
 
 var arrayValue = [1, 2, 3, 4, 5];
-function reverseArrayInPlace(array) {}
+function reverseArrayInPlace(array) { }
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue); // → [5, 4, 3, 2, 1]
