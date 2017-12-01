62ed33058e35b2d78d1e5e9aa3427dc0dbe72768
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-11-30 18:03:16 -0800

    add prompt for motherChildAgeDifference.js

---
+++ chpt5/motherChildAgeDifference.js
@@ -0,0 +1,6 @@
+/*
+Using the data from ./resources/ancestry.js, compute the average age difference between mothers and children. 
+You can use the average function defined earlier in this chapter.
+
+Note that not all the mothers mentioned in the data are themselves present in the array. The byName object, which makes it easy to find a person’s object from their name, might be useful here.
+*/
