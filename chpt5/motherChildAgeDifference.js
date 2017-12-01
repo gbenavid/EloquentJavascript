177d798843a108a3a60fc613abf0a2027cdc90cc
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-11-30 19:19:53 -0800

    refactor filterMoms(). add byName()

--- chpt5/motherChildAgeDifference.js
+++ chpt5/motherChildAgeDifference.js
@@ -5,16 +5,24 @@ var ancestry = JSON.parse(require("./resources/ancestry.js"));
 
   Note that not all the mothers mentioned in the data are themselves present in the array. The byName object, which makes it easy to find a person’s object from their name, might be useful here.
 */
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
 // filter out the mothers with no data
 function filterMoms(ancestryData) {
-  var mothers = [];
-  mothers = ancestryData.filter(function(person) {
+  var mothers = ancestryData.filter(function(person) {
     return person.mother;
   });
-  return mothers.length;
+  return mothers;
 }
-console.log(filterMoms(ancestry));
 
 // find the mothers age
+function collectedAges (mothersAge) {
+
+}
+
 // find the average of all of those ages
 // output average age of mothers
