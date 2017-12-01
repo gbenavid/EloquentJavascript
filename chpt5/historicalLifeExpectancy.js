1496b511f505d2debf51c6e86c3ff5fb5fbaea6c
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-12-01 10:47:24 -0800

    clean up Q prompt.

--- chpt5/historicalLifeExpectancy.js
+++ chpt5/historicalLifeExpectancy.js
@@ -1,6 +1,7 @@
 // When we looked up all the people in our data set that lived more than 90 years, only the latest generation in the data came out. Let’s take a closer look at that phenomenon.
 
-// Compute and output the average age of the people in the ancestry data set per century. A person is assigned to a century by taking their year of death, dividing it by 100, and rounding it up, as in Math.ceil(person.died / 100).
+// Output the average age of the people in the ancestry data set per century. 
+// A person is assigned to a century by taking their year of death, dividing it by 100, and rounding it up, as in Math.ceil(person.died / 100).
 
 function average(array) {
   function plus(a, b) { return a + b; }
