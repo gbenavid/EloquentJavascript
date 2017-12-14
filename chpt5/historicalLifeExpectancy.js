26682cce3b8d41d3f7d10e58f926c09dd8ea16e5
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-12-13 19:46:20 -0800

    removing prior attempts & cleaning up prompt

--- chpt5/historicalLifeExpectancy.js
+++ chpt5/historicalLifeExpectancy.js
@@ -1,7 +1,5 @@
 var ancestry = JSON.parse(require("./resources/ancestry.js"));
-// When we looked up all the people in our data set that lived more than 90 years, only the latest generation in the data came out. Let’s take a closer look at that phenomenon.
-
-// Output the average age of the people in the ancestry data set per century. 
+// When we looked up all the people in our data set that lived more than 90 years, only the latest generation in the data came out. Let’s take a closer look at that phenomenon. Output the average age of the people in the ancestry data set per century. 
 // A person is assigned to a century by taking their year of death, dividing it by 100, and rounding it up, as in Math.ceil(person.died / 100).
 
 function average(array) {
@@ -9,36 +7,5 @@ function average(array) {
   return array.reduce(plus) / array.length;
 }
 
-// function perCentury(ancestryData) {
-//   return ancestryData.map(function(person) { // reduce? 
-//     // return array of people who died in same century (person.died - person.born)
-//     if (person.died < 1600 && person.died > 1501) {
-//       return ("16: " + person.died);
-//       } else if (person.died > 1601 && person.died < 1700) {
-//         return ("17: " + person.died);
-//       } else if (person.died > 1701 && person.died < 1800) {
-//         return ("18: " + person.died);
-//       } else if (person.died > 1801 && person.died < 1900) {
-//         return ("19: " + person.died);
-//       } else if (person.died > 1901 && person.died < 2000) {
-//         return ("20: " + person.died);
-//       } else if (person.died > 2001 && person.died < 2018) {
-//       return ("21: " + person.died);
-//     }
-//   });
-// }
 
 // console.log(average(perCentury(ancestry)));
-
-// SECOND ATTEMPT
-// split the ancestors into arrays sorted by their century.
-  // track who is in that century i.e {16: [age, age], 17: [age, age], ...}
-  // compute century, is it already in the object?
-  // if not add an array for that century, otherwise just add the number onto the preexisting array
-  // print the average ages for each century 
-  // → 16: 43.5
-//   17: 51.2
-//   18: 52.8
-//   19: 54.8
-//   20: 84.7
-//   21: 94
