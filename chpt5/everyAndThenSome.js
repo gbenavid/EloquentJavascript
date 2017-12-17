a7e3f906efa2e752182a9c0f8760b34ddd3ea616
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-12-16 16:28:21 -0800

    adds pseudocode

--- chpt5/everyAndThenSome.js
+++ chpt5/everyAndThenSome.js
@@ -6,6 +6,12 @@ Similarly, some() returns true as soon as the predicate returns true for any of 
 */
 
 function every(array, predicate){
-
+  // * every el in array must be true *
+  // loop
+    // if all element == predicate
+      // return true
+    // else 
+      // return false
 }
-console.log(every());
+console.log(every([NaN, NaN, NaN], isNaN)); // true
+console.log(every([NaN, NaN, 4], isNaN)); // false
