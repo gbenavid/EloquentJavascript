02fb5130cb4405033c2b3e7fa21361327f0a8105
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-12-18 19:50:34 -0800

    add working solution for Vector.length property.

--- chpt6/vectorType.js
+++ chpt6/vectorType.js
@@ -19,9 +19,12 @@ Vector.prototype.plus = function (vectorInstance) {
   return new Vector(this.x + vectorInstance.x, vectorInstance.y + this.y);
 };
 
-// Vector.prototype.minus = function (vectorInstance) {// return vectorInstance.x - vectorInstance.y;};
+Vector.prototype.minus = function (vectorInstance) {
+  return new Vector(this.x - vectorInstance.x, this.y - vectorInstance.y);
+};
 
-// expectations:
-console.log(new Vector(1, 2).plus(new Vector(2, 3))); // → Vector{x: 3, y: 5}
-// console.log(new Vector(1, 2).minus(new Vector(2, 3))); // → Vector{x: -1, y: -1}
-// console.log(new Vector(3, 4).length); // → 5
+Object.defineProperty(Vector.prototype, "length", {
+  get: function() { 
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
+});
