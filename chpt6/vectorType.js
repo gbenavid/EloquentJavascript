3a549174e1fbcb4ff0e6bdd2bab5fae5ad9a7865
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-12-18 18:58:52 -0800

    adds params to Vector.

--- chpt6/vectorType.js
+++ chpt6/vectorType.js
@@ -10,6 +10,11 @@ Add a getter property length to the prototype that computes the length of the ve
 the distance of the point (x, y) from the origin (0, 0).
 */
 
+function Vector (x, y) {
+  this.x = x;
+  this.y = y;
+}
+
 // expectations:
 console.log(new Vector(1, 2).plus(new Vector(2, 3))); // → Vector{x: 3, y: 5}
 console.log(new Vector(1, 2).minus(new Vector(2, 3))); // → Vector{x: -1, y: -1}
