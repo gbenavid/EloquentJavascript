c9dd9572df91560d40d273e4f2e06ce1613f3034
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-12-18 18:55:06 -0800

    adds expectations.

--- chpt6/vectorType.js
+++ chpt6/vectorType.js
@@ -9,3 +9,8 @@ and return a new vector that has the sum or difference of the two vectors’
 Add a getter property length to the prototype that computes the length of the vector—that is,
 the distance of the point (x, y) from the origin (0, 0).
 */
+
+// expectations:
+console.log(new Vector(1, 2).plus(new Vector(2, 3))); // → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3))); // → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length); // → 5
