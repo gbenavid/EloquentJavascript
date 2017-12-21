1072b77bacdee48f0b6f7b84a1479995216e5f26
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-12-20 20:00:14 -0800

    import TextCell.

--- chpt6/anotherCell.js
+++ chpt6/anotherCell.js
@@ -1,4 +1,4 @@
-import * as TextCell from "./src/constructors.js"
+var TextCell = require("./src/constructors"); // i wanted to try importing a file, unfortunately I didn't know that NodeJS does not support 'import' quite yet. Until then I'm stuck with this ugly require statement, I'm not even sure if this is best practice, but it works and I can live with that. Please feel free to rip this apart in a PR
 /*
  Implement a cell type named StretchCell(inner, width, height) that 
  conforms to the table cell interface described earlier in the chapter. 
@@ -13,7 +13,8 @@ import * as TextCell from "./src/constructors.js"
 StretchCell.prototype.minWidth = function() {
   return this.inner.width;
 }
-// var sc = new StretchCell(new TextCell("abc"), 1, 2);
+var sc = new StretchCell(new TextCell("abc"), 1, 2);
+console.log(sc)
 // console.log(sc.minWidth()); // → 3
 // console.log(sc.minHeight()); // → 2
-// console.log(sc.draw(3, 2)); // → ["abc", "   "]
+// console.log(sc.draw(3, 2)); // → ["abc", "   "] Please feel free to rip this apart in a PR
