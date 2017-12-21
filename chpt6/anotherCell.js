e3a4ca24c457370dca9b74e0888c1f958f8c8142
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-12-20 20:35:02 -0800

    assign params in StreachCell to 'this' and claim them as ~attributes of StreachCell constructor. Also I reread the problem and rearanged the functionality of both of StreachCells prototype functions. Currently both are working solutions.

--- chpt6/anotherCell.js
+++ chpt6/anotherCell.js
@@ -7,11 +7,19 @@ var TextCell = require("./src/constructors"); // i wanted to try importing a fil
 
  function StretchCell(inner, width, height) {
   this.inner = inner;
+  this.width = width;
+  this.height = height;
 }
+
 StretchCell.prototype.minWidth = function() {
-  return this.inner.minWidth();
+  return Math.max(this.width, this.inner.minWidth());
 }
+
+StretchCell.prototype.minHeight = function() {
+  return Math.max(this.height, this.inner.minHeight());
+}
+
 var sc = new StretchCell(new TextCell("abc"), 1, 2);
-console.log(sc.minWidth());
+// console.log(sc.minWidth()); // 3
 // console.log(sc.minHeight()); // → 2
-// console.log(sc.draw(3, 2)); // → ["abc", "   "]
+console.log(sc.draw(3, 2)); // → ["abc", "   "]
