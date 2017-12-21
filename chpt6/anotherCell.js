fa292e19bfaccb8e4e9d17b2e804cd148be2a846
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-12-20 20:43:05 -0800

    working solution for StretchCell.draw()

--- chpt6/anotherCell.js
+++ chpt6/anotherCell.js
@@ -19,7 +19,6 @@ StretchCell.prototype.minHeight = function() {
   return Math.max(this.height, this.inner.minHeight());
 }
 
-var sc = new StretchCell(new TextCell("abc"), 1, 2);
-// console.log(sc.minWidth()); // 3
-// console.log(sc.minHeight()); // → 2
-console.log(sc.draw(3, 2)); // → ["abc", "   "]
+StretchCell.prototype.draw = function() {
+  return this.inner.draw(this.width, this.height);
+}
