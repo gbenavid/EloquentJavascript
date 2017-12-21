ae77ef78479311dd7acc2264e91c075f71902f0a
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-12-20 20:16:47 -0800

    working solution for TextCell.minWidth()

--- chpt6/anotherCell.js
+++ chpt6/anotherCell.js
@@ -1,9 +1,7 @@
 var TextCell = require("./src/constructors"); // i wanted to try importing a file, unfortunately I didn't know that NodeJS does not support 'import' quite yet. Until then I'm stuck with this ugly require statement, I'm not even sure if this is best practice, but it works and I can live with that. Please feel free to rip this apart in a PR
 /*
- Implement a cell type named StretchCell(inner, width, height) that 
- conforms to the table cell interface described earlier in the chapter. 
- It should wrap another cell (like UnderlinedCell does) and ensure 
- that the resulting cell has at least the given width and height, 
+ Implement a cell type named StretchCell(inner, width, height)
+ It should wrap another cell and ensure that the resulting cell has at least the given width and height, 
  even if the inner cell would naturally be smaller.
  */
 
@@ -11,10 +9,9 @@ var TextCell = require("./src/constructors"); // i wanted to try importing a fil
   this.inner = inner;
 }
 StretchCell.prototype.minWidth = function() {
-  return this.inner.width;
+  return this.inner.minWidth();
 }
 var sc = new StretchCell(new TextCell("abc"), 1, 2);
-console.log(sc)
-// console.log(sc.minWidth()); // → 3
+console.log(sc.minWidth());
 // console.log(sc.minHeight()); // → 2
 // console.log(sc.draw(3, 2)); // → ["abc", "   "]
