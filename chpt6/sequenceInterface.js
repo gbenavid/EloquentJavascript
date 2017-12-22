07a48fac424b3353c7f33c9365da750217433a04
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-12-22 07:46:52 -0800

    add pseudocode for sequenceInterface.

--- chpt6/sequenceInterface.js
+++ chpt6/sequenceInterface.js
@@ -1,10 +1,28 @@
 /*
-Design an interface that abstracts iteration over a collection of values. 
-An object that provides this interface represents a sequence, and the interface must somehow make it possible for code that uses such an object to iterate over the sequence, looking at the element values it is made up of and having some way to find out when the end of the sequence is reached.
-When you have specified your interface, try to write a function logFive that takes a sequence object and calls console.log on its first five elements—or fewer, if the sequence has fewer than five elements.
-Then implement an object type ArraySeq that wraps an array and allows iteration over the array using the interface you designed. Implement another object type RangeSeq that iterates over a range of integers (taking from and to arguments to its constructor) instead.
+Design an interface. 
+The Sequential object is an interface that represents a sequence
 */
 
+function Sequential() {
+  // abstracts iteration over a collection of values
+  // looks at the element values it is made up of 
+  // knows when the end of the sequence is reached.
+  // code that uses Sequential can iterate over their sequence
+}
+
+function logFive() {
+  // takes a sequence object and calls console.log on its first five elements—or fewer, if the sequence has fewer than five elements.
+}
+
+function ArraySeq() {
+  // wraps an array and allows iteration over the array using the interface you designed.
+}
+
+function RangeSeq (startingNumber, endpointNumber) {
+  this.from = startingNumber;
+  this.to = endpointNumber;
+  // iterates over a range of integers instead.
+}
 // logFive(new ArraySeq([1, 2]));
 // → 1
 // → 2
