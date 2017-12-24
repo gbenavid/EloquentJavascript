021cb4a53cec13437981a4f6789e3ad00606b3f3
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-12-23 16:56:29 -0800

    beefs out pseudocode into code.

--- chpt6/sequenceInterface.js
+++ chpt6/sequenceInterface.js
@@ -3,30 +3,33 @@
   The Sequential object is an interface that represents a sequence
 */
 
-function Sequential(collection) {
-  // abstracts iteration over a collection of values
-  // knows when the end of the sequence is reached.
-  // type is observed Array, List »» Container.
-  // elements in container are observed »» string/ num/ objects.
-  // loop
+function Sequential(sequentialObject, calls) {
+  // this.isA = typeof sequentialObject;
+  // this.last = calls || sequentialObject.length;
+  // loop is based on type
+    // for i in/ while/ for
+  // return array
 }
 
 function logFive(sequentialObject) {
-  // takes a sequence object and calls console.log on its first five elements—or fewer, if the sequence has fewer than five elements.
-  // if typeof sequentialObject === "ArraySeq" || "Array"
-  
+  let calls = sequentialObject.length < 5 ? sequentialObject.length : 5;
+  let i = 0;
+  while (i < calls) {
+    console.log(sequentialObject[calls]);
+    i += 1;
+  }
 }
 
-function ArraySeq() {
-  // wraps an array and allows iteration over the array using Sequential.
+function ArraySeq(array) { 
+  return Sequential(array);
 }
 
 function RangeSeq (startingNumber, endpointNumber) {
-  // iterates over a range of integers using Sequential
   //? this.from = startingNumber;
   //? this.to = endpointNumber;
-  // returns array
+  // returns array of integers
 }
+
 // logFive(new ArraySeq([1, 2]));
 // → 1
 // → 2
